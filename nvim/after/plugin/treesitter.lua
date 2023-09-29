9aa340c17165314819319e32df985ca40d286473
lopqto <morpix@protonmail.com>
2023-09-29 10:29:20 +0330

    Update treesitter's configuration

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,7 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "go", "bash", "yaml", "make", "json", "toml", "rust" },
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "go", "bash", "yaml", "make", "json", "toml",
+        "rust", "haskell", "sql", "dockerfile", "csv" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
