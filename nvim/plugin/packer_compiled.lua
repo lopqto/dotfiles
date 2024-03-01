27bf7fad670d4aa09c595459868c5016b0e5815f
lopqto <morpix@protonmail.com>
2024-03-01 14:44:38 +0330

    Remove default comments

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/lopqto/.cache/nvim/packer_hererocks/2.1.1694285958/share/lua/5.1/?.lua;/home/lopqto/.cache/nvim/packer_hererocks/2.1.1694285958/share/lua/5.1/?/init.lua;/home/lopqto/.cache/nvim/packer_hererocks/2.1.1694285958/lib/luarocks/rocks-5.1/?.lua;/home/lopqto/.cache/nvim/packer_hererocks/2.1.1694285958/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/lopqto/.cache/nvim/packer_hererocks/2.1.1694285958/lib/lua/5.1/?.so"
+local package_path_str = "/Users/lopqto/.cache/nvim/packer_hererocks/2.1.1703358377/share/lua/5.1/?.lua;/Users/lopqto/.cache/nvim/packer_hererocks/2.1.1703358377/share/lua/5.1/?/init.lua;/Users/lopqto/.cache/nvim/packer_hererocks/2.1.1703358377/lib/luarocks/rocks-5.1/?.lua;/Users/lopqto/.cache/nvim/packer_hererocks/2.1.1703358377/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/lopqto/.cache/nvim/packer_hererocks/2.1.1703358377/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,97 +76,97 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["copilot.vim"] = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/copilot.vim",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/copilot.vim",
     url = "https://github.com/github/copilot.vim"
   },
   gruvbox = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/gruvbox",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/gruvbox",
     url = "https://github.com/ellisonleao/gruvbox.nvim"
   },
   harpoon = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/harpoon",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/theprimeagen/harpoon"
   },
   ["lsp-zero.nvim"] = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   melange = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/melange",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/melange",
     url = "https://github.com/savq/melange-nvim"
   },
   nightfly = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/nightfly",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/nightfly",
     url = "https://github.com/bluz71/vim-nightfly-colors"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   oxocarbon = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/oxocarbon",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/oxocarbon",
     url = "https://github.com/nyoom-engineering/oxocarbon.nvim"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   sonokai = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/sonokai",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/sonokai",
     url = "https://github.com/sainnhe/sonokai"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   tokyonight = {
     loaded = true,
-    path = "/home/lopqto/.local/share/nvim/site/pack/packer/start/tokyonight",
+    path = "/Users/lopqto/.local/share/nvim/site/pack/packer/start/tokyonight",
     url = "https://github.com/folke/tokyonight.nvim"
   }
 }
