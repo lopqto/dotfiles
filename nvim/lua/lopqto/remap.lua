207f091cffabadfc81012b703bf3d3118cfcb084
lopqto <morpix@protonmail.com>
2025-04-05 15:20:08 +0330

    Add diagnostics-related shortcuts to neovim

--- nvim/lua/lopqto/remap.lua
+++ nvim/lua/lopqto/remap.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.api.nvim_set_keymap('v', '<Tab>', '>gv', { noremap = true })
 vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>l', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
